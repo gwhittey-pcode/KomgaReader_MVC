e823b5a62cc747acc8b6ebbc72e02ac394ffd76e
Gerard Whittey <gerard.whittey@gmail.com>
2022-10-30 20:50:30 -0400

    merge main

--- Utility/filter_menu.py
+++ Utility/filter_menu.py
@@ -1,42 +1,28 @@
 from kivymd.app import MDApp
 from kivymd.uix.menu import MDDropdownMenu
-
 from Utility.komga_server_conn import ComicServerConn
 
 
 def filter_menu_build():
     screen = MDApp.get_running_app().manager_screens.current_screen
-    publisher_menu = MDDropdownMenu()
-
+    item_per_menu_items = []
     def __got_publisher_data(results):
+
         filter_menu_items = results
-        publisher_menu_items = []
-        screen.filter_menu = MDDropdownMenu()
         for item in filter_menu_items:
-            publisher_menu_items.append(
+            item_per_menu_items.append(
                 {
                     "text": f"{item}",
-                    "viewclass": "MDDropdownMenu",
-                    "on_release": lambda x="Publisher": filter_menu_callback(x),
+                    "viewclass": "ListItemWithCheckbox",
+                    "on_release": lambda x=f"{item}": screen.filter_menu_callback(x),
                 }
             )
-
-            publisher_menu.caller = screen.filter_menu
-            publisher_menu.items = publisher_menu_items
-            publisher_menu.width_mult = 5
+        screen.filter_menu = MDDropdownMenu(
+            caller=screen.ids.filter_menu_button,
+            items=item_per_menu_items,
+            width_mult=5,
             # max_height=dp(240)
-
-        item_per_menu_items = [{
-            "text": f"Publisher",
-            "viewclass": publisher_menu,
-            "on_release": lambda x="Publisher": filter_menu_callback(x),
-        }]
-
-        screen.filter_menu.caller=screen.ids.filter_menu_button
-        screen.filter_menu.items=item_per_menu_items
-        screen.filter_menu.width_mult=5
-        # max_height=dp(240)
-
+        )
 
     fetch_data = ComicServerConn()
     url_send = f"{screen.base_url}/api/v1/publishers"
@@ -44,6 +30,5 @@ def filter_menu_build():
         url_send,
         callback=lambda url_send, results: __got_publisher_data(results))
 
-    def filter_menu_callback(text_item):
-        publisher_menu.open()
-        screen.filter_menu.dismiss()
+    def filter_menu_callback(self, text_item):
+        self.filter_menu.dismiss()
